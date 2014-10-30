48322c96d29122a7262ab6f5ff310d322e014b63
adobrov <andrei2710@yandex.ru>
2014-10-31 01:15:29 +0600

    fix types

--- visitors/visit_from_postres_types.py
+++ visitors/visit_from_postres_types.py
@@ -2,13 +2,19 @@ from sqlalchemy import schema
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.dialects.postgresql.base import OID
 from sqlalchemy.dialects.postgresql.base import TEXT
+from sqlalchemy.dialects.postgresql.base import BOOLEAN
+from sqlalchemy.dialects.postgresql.base import BIGINT
 
 @compiles(schema.CreateColumn)
 def visit_from_postgres_types(element, compiler, **kw):
     column = element.element
     if isinstance(column.type, OID):
         return '%s BLOB' % column.name
-    if isinstance(column.type, TEXT):
+    elif isinstance(column.type, TEXT):
         return '%s CLOB' % column.name
+    elif isinstance(column.type, BOOLEAN):
+        return '%s INTEGER(1,0)' % column.name
+    elif isinstance(column.type, BIGINT):
+        return '%s NUMBER(19,0)' % column.name
     else:
         return compiler.visit_create_column(element, **kw)
